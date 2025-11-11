08093a87041d61bfdc761527a4112340974e5445
Mixaxa1 <omm15062003@gmail.com>
2025-11-11 13:34:20 +0200

    test pull request commit 2

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,6 +6,7 @@ internal static class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        // Add services to the container.
         builder.Services.AddControllers();
         builder.Services.AddDb();
         var app = builder.Build();
